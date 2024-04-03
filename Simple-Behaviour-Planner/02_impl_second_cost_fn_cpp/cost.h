5b31c4670d7d6dc480d9dbde291cc6d4cec3504e
Nitish Sanghi <nitishsanghi@gmail.com>
2024-04-03 15:48:32 -0700

    Clearn up

--- Simple-Behaviour-Planner/02_impl_second_cost_fn_cpp/cost.h
+++
@@ -1,9 +0,0 @@
-#ifndef COST_H
-#define COST_H
-
-#include <vector>
-
-double inefficiency_cost(int target_speed, int intended_lane, int final_lane, 
-                         const std::vector<int> &lane_speeds);
-
-#endif  // COST_H
